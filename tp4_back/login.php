b333f94fa894151a6fd71b57229ce995890f8ae3
Antonella Lepe <90052974+1-2LKKJJGYHJ@users.noreply.github.com>
2022-04-28 11:34:52 -0300

    Add files via upload

---
+++ tp4_back/login.php
@@ -0,0 +1,14 @@
+<?php
+$user = $_POST  ["user"]
+$pass = $_POST  ["pass"]
+
+$usuario = "Antonella lepe";
+$contraseña = "1234";
+
+if ($usuario == $user && $contraseña == $pass) {
+    echo (correcto);
+    # code...
+} else {
+    echo (incorrecto);
+
+?>
